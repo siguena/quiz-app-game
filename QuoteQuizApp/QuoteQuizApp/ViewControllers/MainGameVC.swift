acc45537ac34a97cf8e5e1578a6329bb6af1392a
Vladimir Milushev <vladimirmilushev@Vladimirs-MacBook-Air.local>
2017-11-18 09:37:02 +0200

    Interface set up

---
+++ QuoteQuizApp/QuoteQuizApp/ViewControllers/MainGameVC.swift
@@ -0,0 +1,25 @@
+//
+//  MainGameVC.swift
+//  QuoteQuizApp
+//
+//  Created by Vladimir Milushev on 18.11.17.
+//  Copyright © 2017 Vladimir Milushev. All rights reserved.
+//
+
+import UIKit
+
+class MainGameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
